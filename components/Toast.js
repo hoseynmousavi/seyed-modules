943830cb562a36ae9da59571a3e22744c3ff39f5
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-08-21 15:39:39 +0430

    some little fix

--- components/Toast.js
+++ components/Toast.js
@@ -89,7 +89,7 @@ function Toast({item: {id, message, type, onClick, isUndo}, clearMe})
     }
 
     return (
-        <div className={`toast-item ${isDark ? "dark" : ""} ${type}`} ref={toastRef} style={{height: "0", opacity: "0", marginBottom: "0", padding: "0 16px"}} onTouchEnd={!(isUndo || onClick) && clearItem} onClick={onClick ? onClickFunc : clearItem}>
+        <div className={`toast-item ${isDark ? "dark" : ""} ${type}`} ref={toastRef} style={{height: "0", opacity: "0", marginBottom: "0", padding: "0 16px"}} onTouchEnd={!(isUndo || onClick) ? clearItem : null} onClick={onClick ? onClickFunc : clearItem}>
             <div className="toast-item-message" ref={toastMessageRef}>
                 {
                     isUndo ?

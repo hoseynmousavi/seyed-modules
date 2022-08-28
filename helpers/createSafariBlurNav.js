e13e3bc6d044b27a87d482cd5c458d7932350dc3
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-08-28 16:55:11 +0430

    added safari blur

--- helpers/createSafariBlurNav.js
+++ helpers/createSafariBlurNav.js
@@ -3,7 +3,11 @@ import checkOs from "./checkOs"
 
 function createSafariBlurNav({color, variable, alpha = 0.82})
 {
-    return createMaterialColor({color, variable, alpha: checkOs() === "mac" || checkOs() === "ios" ? alpha : 1})
+    const isApple = checkOs() === "mac" || checkOs() === "ios"
+    return ({
+        backgroundColor: createMaterialColor({color, variable, alpha: isApple ? alpha : 1}),
+        backdropFilter: isApple ? "saturate(100%) blur(20px)" : "none",
+    })
 }
 
 export default createSafariBlurNav

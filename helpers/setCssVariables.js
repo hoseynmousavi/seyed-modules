5dba54fcbd0d8adc5b23b1f91ad42631aa3b9217
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-09-03 16:38:06 +0430

    fix ios rotate bug

--- helpers/setCssVariables.js
+++ helpers/setCssVariables.js
@@ -2,22 +2,27 @@ import onResize from "./onResize"
 
 function setCssVariables()
 {
+    let timeout = null
     const viewport = +process.env.REACT_APP_DESKTOP_VIEWPORT.replace("px", "")
     fitVariables()
 
     function fitVariables()
     {
-        const clientWidth = window.innerWidth
-        const clientHeight = window.innerHeight
+        clearTimeout(timeout)
+        timeout = setTimeout(() =>
+        {
+            const clientWidth = window.innerWidth
+            const clientHeight = window.innerHeight
 
-        document.documentElement.style.setProperty(
-            "--full-viewport",
-            clientWidth > viewport ? viewport + "px" : "100vw",
-        )
-        document.documentElement.style.setProperty(
-            "--full-height",
-            clientHeight + "px",
-        )
+            document.documentElement.style.setProperty(
+                "--full-viewport",
+                clientWidth > viewport ? viewport + "px" : "100vw",
+            )
+            document.documentElement.style.setProperty(
+                "--full-height",
+                clientHeight + "px",
+            )
+        }, 100)
     }
 
     onResize({callback: fitVariables})

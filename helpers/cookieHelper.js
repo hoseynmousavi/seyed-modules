df68680ff1206059440393b67d6e0a04b92459f3
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-09-21 17:23:13 +0430

    fix use either cookie or local

--- helpers/cookieHelper.js
+++ helpers/cookieHelper.js
@@ -8,35 +8,44 @@ function makeKeyOnEnv(key)
 
 function setItem(key, value, expireDay = 365)
 {
-    const saveKey = makeKeyOnEnv(key)
-    const d = new Date()
-    d.setTime(d.getTime() + (expireDay * 24 * 60 * 60 * 1000))
-    document.cookie = `${saveKey}=${value}; SameSite=Lax; Secure; domain=${process.env.NODE_ENV === "development" ? "localhost" : process.env.REACT_APP_DOMAIN}; path=/; Priority=High; expires=${d.toUTCString()};`
+    if (window.location.protocol === "http:") localStorage.setItem(key, value)
+    else
+    {
+        const saveKey = makeKeyOnEnv(key)
+        const d = new Date()
+        d.setTime(d.getTime() + (expireDay * 24 * 60 * 60 * 1000))
+        document.cookie = `${saveKey}=${value}; SameSite=Lax; Secure; domain=${process.env.NODE_ENV === "development" ? "localhost" : process.env.REACT_APP_DOMAIN}; path=/; Priority=High; expires=${d.toUTCString()};`
+    }
 }
 
 function removeItem(key)
 {
-    setItem(key, "")
+    if (window.location.protocol === "http:") localStorage.removeItem(key)
+    else setItem(key, "")
 }
 
 function getItem(key)
 {
-    const saveKey = makeKeyOnEnv(key)
-    let name = saveKey + "="
-    let ca = document.cookie.split(";")
-    for (let i = 0; i < ca.length; i++)
+    if (window.location.protocol === "http:") return localStorage.getItem(key)
+    else
     {
-        let c = ca[i]
-        while (c.charAt(0) === " ")
+        const saveKey = makeKeyOnEnv(key)
+        let name = saveKey + "="
+        let ca = document.cookie.split(";")
+        for (let i = 0; i < ca.length; i++)
         {
-            c = c.substring(1)
+            let c = ca[i]
+            while (c.charAt(0) === " ")
+            {
+                c = c.substring(1)
+            }
+            if (c.indexOf(name) === 0)
+            {
+                return c.substring(name.length, c.length)
+            }
         }
-        if (c.indexOf(name) === 0)
-        {
-            return c.substring(name.length, c.length)
-        }
+        return ""
     }
-    return ""
 }
 
 const cookieHelper = {

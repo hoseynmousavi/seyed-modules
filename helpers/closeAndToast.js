302866c5b3b2a0e3b19bc7c27161864ee19c94c5
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-09-26 08:31:15 +0330

    added closeAndToast module

--- helpers/closeAndToast.js
+++ helpers/closeAndToast.js
@@ -1,7 +1,7 @@
 import goBack from "./goBack"
 import toastManager from "./toastManager"
 
-function closeAndToast({back: {fallback, delta}, toast: {message, type, isUndo, onClick, removeOnChangeLocation}})
+function closeAndToast({back: {fallback, delta} = {}, toast: {message, type, isUndo, onClick, removeOnChangeLocation}})
 {
     goBack({fallback, delta})
     setTimeout(() => toastManager.addToast({message, type, isUndo, onClick, removeOnChangeLocation}), 150)

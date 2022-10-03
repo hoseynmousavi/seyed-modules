b19f2bc753f1f0feb5fd0b95038241898aeb28ef
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-10-03 19:23:51 +0330

    add scroll y root

--- hooks/ScrollY.js
+++ hooks/ScrollY.js
@@ -1,18 +1,18 @@
 import {useEffect} from "react"
 
-function ScrollY({condition, updateParams = []})
+function ScrollY({rootId = "main-render", condition, updateParams = []})
 {
     useEffect(() =>
     {
-        const root = document.getElementById("main-render")
+        const root = document.getElementById(rootId)
 
         function onScroll()
         {
             condition?.({scrollTop: root.scrollTop, scrollHeight: root.scrollHeight})
         }
 
-        root.addEventListener("scroll", onScroll, {passive: true})
-        return () => root.removeEventListener("scroll", onScroll)
+        root?.addEventListener?.("scroll", onScroll, {passive: true})
+        return () => root?.removeEventListener?.("scroll", onScroll)
         // eslint-disable-next-line
     }, updateParams)
 }

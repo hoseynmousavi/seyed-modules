07dbf844530e00366b539fb09b0af1295b6b86eb
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-10-17 14:01:00 +0330

    lang

--- hooks/GetTextConstant.js
+++ hooks/GetTextConstant.js
@@ -2,13 +2,20 @@ import {useContext} from "react"
 import {LanguageContext} from "../context/language/LanguageReducer"
 import faTextConstant from "../../constant/faTextConstant"
 import enTextConstant from "../../constant/enTextConstant"
+import LanguageActions from "../context/language/LanguageActions"
 
 function GetTextConstant()
 {
-    const {state: {language}} = useContext(LanguageContext)
+    const {state: {language}, dispatch} = useContext(LanguageContext)
     const textConstant = language === "fa" ? faTextConstant : enTextConstant
     const direction = language === "fa" ? "rtl" : "ltr"
-    return {textConstant, language, direction}
+
+    function changeLang({language, changeVariables})
+    {
+        LanguageActions.changeLanguage({language, changeVariables, dispatch})
+    }
+
+    return {textConstant, language, direction, changeLang}
 }
 
 export default GetTextConstant

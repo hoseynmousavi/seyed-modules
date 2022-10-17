81b60f51098fb75df2a6e8101b5c3fc35f52518f
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-10-17 18:00:59 +0330

    toast lang

--- hooks/GetTextConstant.js
+++ hooks/GetTextConstant.js
@@ -2,12 +2,15 @@ import {useContext} from "react"
 import {LanguageContext} from "../context/language/LanguageReducer"
 import faTextConstant from "../../constant/faTextConstant"
 import enTextConstant from "../../constant/enTextConstant"
+import enToastConstant from "../../constant/enToastConstant"
+import faToastConstant from "../../constant/faToastConstant"
 import LanguageActions from "../context/language/LanguageActions"
 
 function GetTextConstant()
 {
     const {state: {language}, dispatch} = useContext(LanguageContext)
     const textConstant = language === "fa" ? faTextConstant : enTextConstant
+    const toastConstant = language === "fa" ? faToastConstant : enToastConstant
     const direction = language === "fa" ? "rtl" : "ltr"
 
     function changeLang({language, changeVariables})
@@ -15,7 +18,7 @@ function GetTextConstant()
         LanguageActions.changeLanguage({language, changeVariables, dispatch})
     }
 
-    return {textConstant, language, direction, changeLang}
+    return {textConstant, toastConstant, language, direction, changeLang}
 }
 
 export default GetTextConstant

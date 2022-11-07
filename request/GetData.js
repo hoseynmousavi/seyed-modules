789735dca8f2c1e3b91f7b7807ba0f9bd05b38c1
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-07 12:09:29 +0330

    revert data

--- request/GetData.js
+++ request/GetData.js
@@ -1,7 +1,7 @@
 import {useEffect} from "react"
 import {REQUEST_CANCEL} from "../constant/toastTypes"
 
-function GetData({request, isLoading, cancelToken, doAfterGet, dependencies = [], forceNetworkReq})
+function GetData({request, isLoading, cancelToken, doAfterGet, dependencies = []})
 {
     useEffect(() =>
     {
@@ -11,7 +11,7 @@ function GetData({request, isLoading, cancelToken, doAfterGet, dependencies = []
             request()?.then?.(() => doAfterGet && setTimeout(doAfterGet, 10))
         }
 
-        if (forceNetworkReq || isLoading) sendRequest()
+        if (isLoading) sendRequest()
         else doAfterGet?.()
 
         window.addEventListener("online", sendRequest)

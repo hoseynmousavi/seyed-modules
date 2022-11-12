4afec98c5e0a69b39d88fcb0f6c3822a37b32903
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-12 09:20:07 +0330

    add click event after img loaded

--- components/ImageLoading.js
+++ components/ImageLoading.js
@@ -10,11 +10,12 @@ const ImageLoading = forwardRef(({className, style, src, alt, loading, onClick, 
         loadedRef.current = true
         contRef.current.className = className
         ref.current.className = `${className} image-loading-main loaded`
+        ref.current.addEventListener("click", onClick)
     }
 
     return (
         <div className={`${className} ${loadedRef.current ? "" : "image-loading"}`} style={style} ref={contRef}>
-            <img decoding="async" draggable={draggable} className={`${className} image-loading-main ${loadedRef.current ? "loaded" : ""}`} onLoad={loaded} style={style} ref={ref} src={src} alt={alt} loading={loading} onClick={onClick}/>
+            <img decoding="async" draggable={draggable} className={`${className} image-loading-main ${loadedRef.current ? "loaded" : ""}`} onLoad={loaded} style={style} ref={ref} src={src} alt={alt} loading={loading}/>
         </div>
     )
 })

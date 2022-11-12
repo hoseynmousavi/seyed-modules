da3099cab5c229ccc646563bf6aa815cd25aac97
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-12 18:05:00 +0330

    added get domain

--- helpers/cookieHelper.js
+++ helpers/cookieHelper.js
@@ -1,3 +1,5 @@
+import getDomain from "./getDomain"
+
 function isHttpOrFile()
 {
     return (window.location.protocol === "http:" || window.location.protocol === "file:")
@@ -16,10 +18,11 @@ function setItem(key, value, expireDay = 365)
     if (isHttpOrFile()) localStorage.setItem(key, value)
     else
     {
+        const domain = getDomain()
         const saveKey = makeKeyOnEnv(key)
         const d = new Date()
         d.setTime(d.getTime() + (expireDay * 24 * 60 * 60 * 1000))
-        document.cookie = `${saveKey}=${value}; SameSite=Lax; Secure; domain=${process.env.NODE_ENV === "development" ? "localhost" : process.env.REACT_APP_DOMAIN}; path=/; Priority=High; expires=${d.toUTCString()};`
+        document.cookie = `${saveKey}=${value}; SameSite=Lax; Secure; domain=${process.env.NODE_ENV === "development" ? "localhost" : domain}; path=/; Priority=High; expires=${d.toUTCString()};`
     }
 }
 

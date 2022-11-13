89b97acd7e96688266e68e613642a6245b932e59
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-13 23:23:07 +0330

    fix

--- helpers/setCssVariables.js
+++ helpers/setCssVariables.js
@@ -11,8 +11,8 @@ function setCssVariables()
     {
         function setStyle()
         {
-            const clientWidth = window.innerWidth + 1
-            const clientHeight = window.innerHeight + 1
+            const clientWidth = window.innerWidth
+            const clientHeight = window.innerHeight
 
             document.documentElement.style.setProperty(
                 "--full-viewport",

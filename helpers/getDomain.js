d3b2278a2897fba2fbdcc899019adea21f085f0d
Hoseyn <hoseyn.mousavi78@gmail.com>
2023-02-07 01:33:19 +0330

    d

--- helpers/getDomain.js
+++ helpers/getDomain.js
@@ -1,7 +1,11 @@
 function getDomain()
 {
-    const hostName = window.location.hostname
-    return hostName.substring(hostName.lastIndexOf(".", hostName.lastIndexOf(".") - 1) + 1)
+    if (process.env.REACT_APP_DOMAIN) return process.env.REACT_APP_DOMAIN
+    else
+    {
+        const hostName = window.location.hostname
+        return hostName.substring(hostName.lastIndexOf(".", hostName.lastIndexOf(".") - 1) + 1)
+    }
 }
 
 export default getDomain
